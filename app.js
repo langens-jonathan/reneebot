98ff9c38c3339b7b412582ddabc3eb3f141fb953
langens-jonathan <flowofcontrol@gmail.com>
2016-06-20 19:56:16 +0200

    remove stupid bot route

--- app.js
+++ app.js
@@ -7,13 +7,10 @@ bot.add('/', function (session) {
     session.send('Hello World');
 });
 
-bot.add('/privacy', function(session){
-    session.send('no privacy');
-});
-
 // Setup Restify Server
 var server = restify.createServer();
 server.post('/api/messages', bot.verifyBotFramework(), bot.listen());
+server.get('/privacy',privacyFunction());
 server.listen(process.env.port || 3978, function () {
     console.log('%s listening to %s', server.name, server.url); 
 });
